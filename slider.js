59dae2ca7ea08c45b08b119be944fe59a466aab1
sidyadav <sid@wizpert.com>
2017-01-18 16:51:05 -0500

    Fix typo

--- slider.js
+++ slider.js
@@ -99,8 +99,7 @@
         ngModel: '=?',
         ngModelLow: '=?',
         ngModelHigh: '=?',
-        
-        Stop: '='
+        onMoveStop: '='
       },
       template: '<div class="bar"><div class="selection"></div></div>\n<div class="handle low"></div><div class="handle high"></div>\n<div class="bubble limit low">{{ values.length ? values[floor || 0] : floor }}</div>\n<div class="bubble limit high">{{ values.length ? values[ceiling || values.length - 1] : ceiling }}</div>\n<div class="bubble value low">{{ values.length ? values[local.ngModelLow || local.ngModel || 0] : local.ngModelLow || local.ngModel || 0 }}</div>\n<div class="bubble value high">{{ values.length ? values[local.ngModelHigh] : local.ngModelHigh }}</div>',
       compile: function(element, attributes) {
